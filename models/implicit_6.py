f641943bab0548440714980df650c14b63b005b3
Harshita Srinivas <harshita19244@iiitd.ac.in>
2022-04-24 18:48:59 +0530

    integrated models

---
+++ models/implicit_6.py
@@ -0,0 +1,30 @@
+from torch import mode
+import pickle
+from keras.preprocessing.sequence import pad_sequences
+import preprocessor as p
+import pandas as pd
+import numpy as np
+import argparse
+from models.model import TextAttBiRNN
+
+def caller(test):
+    max_features = 25000 
+    maxlen = 20
+    batch_size = 512
+    embedding_dims = 50
+    SAVED_DIR = "./saved_models/implicit/6way"
+    data = "implicit"
+    model2 = TextAttBiRNN(maxlen, max_features, embedding_dims, class_num = 3, last_activation= 'softmax')
+    model2.compile('adam', 'categorical_crossentropy', metrics=['accuracy'])
+    model2.load_weights(SAVED_DIR)
+    # test = test.dropna()
+    print(test)
+    x_test = test['Tweet'].to_numpy()
+    #y_test = pd.get_dummies(y_test).to_numpy()
+    token = pickle.load(open(f'{SAVED_DIR}{data}_tokenizer', 'rb'))
+    x_test = pad_sequences(token.texts_to_sequences(x_test), maxlen=maxlen)
+    result = model2.predict(x_test)
+    result = np.argmax(result, axis = 1)
+    test['Category'] = pd.Series(result)
+    print(result)
+    return test
